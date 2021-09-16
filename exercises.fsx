ab3354d59b4f88f3714e0a80a4cdfb0bc9f65627
Yozj <matvei.hedgehog@gmail.com>
2021-09-16 14:50:10 -0400

    exercises.fsx 1.2.1 (list functions 2)

--- exercises.fsx
+++ exercises.fsx
@@ -1,31 +1,11 @@
-let f spisok =
-    let rec g spisok a =
-        match spisok with
-        |h::t -> g t (h + a)
-        |[] -> a
-    let b = g spisok 0
-    b
+// :o :P >:D *u* '_' '-' ;) >:( :() =) :): ~(:{) 8-)
 
-let rec f' lst = 
-    match lst with
-    | [] -> 0
-    | h::t -> h + f t
+let i j =
+    j/42
 
-let fu spisok =
-    let rec g spisok a =
-        match spisok with
-        |h::t -> g t (h * a)
-        |[] -> a
-    g spisok 1
-
-let rec g spisk =
-    match spisk with
+let rec f g h =
+    match h with
+    |a::b -> g a::f g b
     |[] -> []
-    |h::t -> h + 5::g t
-    
-let rec i spsok =
-    match spsok with
-    |[] -> []
-    |h::t -> h * 2::i t
 
-i [21; 210; 42] 
+f (fun j -> j/42) [42; 84; 126; 1764; 42*4]
