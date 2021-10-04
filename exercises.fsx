1ed68529ea63267e8c9014bbcf3190f65378e5af
Yozj <matvei.hedgehog@gmail.com>
2021-10-04 13:52:05 -0400

    exercises.fsx 1.3.2 (list sorting)

--- exercises.fsx
+++ exercises.fsx
@@ -1,115 +1,47 @@
-// :o :P >:D *u* '_' '-' ;) >:( :() =) :): ~(:{) 8-)
-
-let i j =
-    j * j
-
-let rec map g h =
-    match h with
-    |a::b -> g a::map g b
-    |[] -> []
-
-map (fun j -> j * j) [42; 84; 126; 1764; 42*4]
-
-let rec filter h i =
-    match i with
-    |a::b -> match h a with
-                |true -> a::filter h b
-                |false -> filter h b
-    |[] -> []
-
-filter (fun i -> i > 5) [1;2;3;4;5;6;7;8;9;6;3;2;1] = [6;7;8;9;6]
-
-let rec take n l =
-    match n with
-    |0 -> []
-    |_ -> match l with
-            |a::b -> a::take (n - 1) b
-            |[] -> []
-
-let rec length l =
+let rec max l =
     match l with
-    |a::b -> 1 + length b
-    |[] -> 0
-
-take 4 [1;2;4;7;11;16;22;29;37;46;56;67]
-
-let rec drop n l =
-    match n, l with
-    |0, _ -> l
-    |_, [] -> []
-    |_, _::b -> drop (n - 1) b
-
-drop 3 [1;2;3;4;5;6;7;8]
-
-let trytake n l =
-    let x = take n l
-    if length x < n then None else Some x
-
-trytake 42 [1;2;3;4;5;6;7;8;9;10;11;12;13;14]
-
-/// This function returns the first element of the list
-let head l =
-    match l with
-    |a::b -> Some a
+    |a::b::c -> if a < b then max (b::c) else max (a::c)
+    |[a] -> Some a
     |[] -> None
 
-head [1;2;3]
-
-head (drop 3 [1;2;3])
-
-head ["a";"b"]
-
-let rec onlyPresents l =
+let rec min l =
     match l with
-    |Some x :: b -> x::onlyPresents b
-    |None :: b -> onlyPresents b
+    |a::b::c -> if a > b then min (b::c) else min (a::c)
+    |[a] -> Some a
+    |[] -> None
+
+let rec remove e l =
+    match l with
+    |a::b -> if a = e then remove e b else a::remove e b 
     |[] -> []
 
-onlyPresents [Some 1; None; Some 5; Some 42; None] 
+let rec sort l =
+    match min l with
+    |Some x -> x::sort (remove x l)
+    |None -> []
 
-let rec index n l =
-    head (drop n l)
+let rec minBy f l =
+    match l with
+    |a::b::c -> if f a > f b then minBy f (b::c) else minBy f (a::c)
+    |[a] -> Some a
+    |[] -> None
 
-index 3 [1;2;3;42;5;6;7] 
-
-let add5 n =
-    match n with
-    |Some x -> Some (x + 5)
-    |None -> None
-
-add5 (Some 2) = Some 7
-add5 None = None
-add5 (Some 37) = Some 42
-
-let mult2 n =
-    match n with
-    |Some x -> Some (x * 2)
-    |None -> None
-
-mult2 (Some 2) 
-mult2 None
-mult2 (Some 21)
-
-let optionMap f n =
-    match n with
-    |Some x -> Some (f x)
-    |None -> None
-
-optionMap (fun x -> x * 2) (Some 42)
-
-optionMap add5 (Some (Some 42))
-
-let optionToList o =
-    match o with
-    |Some x -> [x]
+let rec sortBy f l =
+    match minBy f l with
+    |Some x -> x::sortBy f (remove x l)
     |None -> []
 
 
-let listToOption l =
-    match l with
-    |[a] -> Some a
-    |_ -> None
+max (["мурч крокодил";"Ёж крокодил"] : string list)
 
-listToOption [42;21]
+min (["мурч крокодил";"ёж крокодил"] : string list)
 
-optionToList (Some 42)
+sort [3.1415926;21.0;42.0;5.25;10.5;84.0;168.0;2.7]
+
+String.length "4242424242424242"
+
+sortBy String.length ["a"; "b"; "abc"; "ab"; "x"] 
+
+remove 36 [42;42;36;42;36;36;42]
+
+minBy f [42;42;42;50]
