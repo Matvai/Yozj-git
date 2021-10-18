00f197fa2217db0dbbb86f6a70d864339e8c7839
Yozj <matvei.hedgehog@gmail.com>
2021-10-18 15:52:21 -0400

    exercies.fsx 4.0 (Game of Life (neighbor counting))

--- exercises.fsx
+++ exercises.fsx
@@ -1,47 +1,17 @@
-let rec max l =
-    match l with
-    |a::b::c -> if a < b then max (b::c) else max (a::c)
-    |[a] -> Some a
-    |[] -> None
+let filterNeighbors (x, y) cells =
+    cells |> List.filter (fun (a, b) -> 
+        a >= x - 1 
+        && a <= x + 1 
+        && b >= y - 1 
+        && b <= y + 1 
+        ) 
 
-let rec min l =
-    match l with
-    |a::b::c -> if a > b then min (b::c) else min (a::c)
-    |[a] -> Some a
-    |[] -> None
+let rec countNeighbors (x, y) cells =
+    cells |> filterNeighbors (x, y) |> List.length
 
-let rec remove e l =
-    match l with
-    |a::b -> if a = e then b else a::remove e b 
-    |[] -> []
-
-let rec sort l =
-    match min l with
-    |Some x -> x::sort (remove x l)
-    |None -> []
-
-let rec minBy f l =
-    match l with
-    |a::b::c -> if f a > f b then minBy f (b::c) else minBy f (a::c)
-    |[a] -> Some a
-    |[] -> None
-
-let rec sortBy f l = // O(N^2)
-    match minBy f l with
-    |Some x -> x::sortBy f (remove x l)
-    |None -> []
-
-
-max (["мурч крокодил";"Ёж крокодил"] : string list)
-
-min (["мурч крокодил";"ёж крокодил"] : string list)
-
-sort [3.1415926;21.0;42.0;5.25;10.5;84.0;168.0;2.7]
-
-String.length "424242424242424242424242424242424242424242"
-
-sortBy String.length ["a"; "b"; "abc"; "ab"; "x"] 
-
-remove 36 [42;42;36;42;36;36;42]
-
-sortBy id [1;4;3;2;6;42;42;42;24;6]
+let gol cell cells =
+    let n = countNeighbors cell cells
+    if n < 2 then dies 
+    else if n > 3 then dies 
+    else if n = 2 then lives 
+    else lives
